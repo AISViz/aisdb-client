694f9873c060d6c74480fa619cbcb53ee551e648
matt24smith <matt24smith@gmail.com>
2022-09-16 10:17:43 -0300

    add command line options for server configuration, and enable logging from multiple sources simultaneously

---
+++ dispatcher/readme.md
@@ -0,0 +1,36 @@
+# Network Dispatcher
+Client/server application to send data from a file descriptor or socket over the network to one or more logging servers
+
+### Features
+- [X] Stream arbitrary data over the network
+- [X] Log data from multiple clients simultaneously
+- [X] Stream to multiple logging servers simultaneously 
+- [X] Minimal (2 dependencies, compiled size ~300kb)
+- [X] Extremely fast
+
+### Compatible with:
+
+- [ ] TCP (Planned feature)
+- [X] UDP
+- [X] IPv4
+- [X] IPv6
+- [X] Unix
+- [X] Windows (untested)
+
+
+
+
+### Server
+
+Start the logging server
+```
+cargo run --bin server --release -- --port 9920 --client_addr 127.0.0.1 --client_addr 127.0.0.2 --path logfile.log
+```
+
+### Client
+
+Stream data from the client to the logging server:
+```
+cargo run --bin client --release -- --port 9920 --listen_addr 0.0.0.0 --path /dev/random
+```
+
--- dispatcher/src/socket.rs
+++ dispatcher/src/socket.rs
@@ -1,18 +1,17 @@
 use std::io;
 use std::net::SocketAddr;
-use std::time::Duration;
+//#[cfg(debug_assertions)]
+//use std::time::Duration;
 
 use socket2::{Domain, Protocol, Socket, Type};
 
-/// On unixes we bind to the multicast address, which causes multicast packets to be filtered
+/// On unix bind to the multicast address
 #[cfg(unix)]
 pub fn bind_socket(socket: &Socket, addr: &SocketAddr) -> io::Result<()> {
     socket.bind(&socket2::SockAddr::from(*addr))
 }
 
-/// On Windows, unlike all Unix variants, it is improper to bind to the multicast address
-///
-/// see https://msdn.microsoft.com/en-us/library/windows/desktop/ms737550(v=vs.85).aspx
+/// https://msdn.microsoft.com/en-us/library/windows/desktop/ms737550(v=vs.85).aspx
 #[cfg(windows)]
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 #[cfg(windows)]
@@ -29,7 +28,6 @@ pub fn bind_socket(socket: &Socket, addr: &SocketAddr) -> io::Result<()> {
     socket.bind(&socket2::SockAddr::from(addr))
 }
 
-// this will be common for all our sockets
 pub fn new_socket(addr: &SocketAddr) -> io::Result<Socket> {
     let domain = if addr.is_ipv4() {
         Domain::IPV4
@@ -41,11 +39,11 @@ pub fn new_socket(addr: &SocketAddr) -> io::Result<Socket> {
 
     let socket = Socket::new(domain, Type::DGRAM, Some(Protocol::UDP))?;
     socket.set_reuse_port(true)?;
-    //socket.set_freebind(true)?;
+    socket.set_freebind(true)?;
 
-    // we're going to use read timeouts so that we don't hang waiting for packets
-    socket.set_read_timeout(Some(Duration::from_millis(100)))?;
-    //socket.set_read_timeout(None)?;
+    socket.set_read_timeout(None)?;
+    //#[cfg(debug_assertions)]
+    //socket.set_read_timeout(Some(Duration::from_millis(100)))?;
 
     Ok(socket)
 }
