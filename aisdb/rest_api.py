013074952d3e08b8f14803910cf456cfdd67d1c5
matt24smith <matt24smith@gmail.com>
2023-09-13 16:57:21 -0300

    rest API error handling

--- aisdb/rest_api.py
+++ aisdb/rest_api.py
@@ -15,7 +15,6 @@ from aisdb import PostgresDBConn, DBQuery
 from flask import (
     Flask,
     Markup,
-    flash,
     request,
     send_file,
 )
@@ -35,21 +34,23 @@ test_db_args = dict(
     password='devel',
 )
 
+default_query = {
+    #'start': int((datetime.utcnow() - timedelta(hours=24)).timestamp()),
+    #'end': int(datetime.utcnow().timestamp()),
+    'start': int(datetime(2021, 7, 1).timestamp()),
+    'end': int(datetime(2021, 7, 30).timestamp()),
+    'xmin': -65,
+    'xmax': -62,
+    'ymin': 43,
+    'ymax': 45,
+}
+
 
 @app.route('/', methods=['GET', 'POST'])
 def download():
     http_qry = dict(request.args)
-    default_query = {
-        #'start': int((datetime.utcnow() - timedelta(hours=24)).timestamp()),
-        #'end': int(datetime.utcnow().timestamp()),
-        'start': int(datetime(2021, 7, 1).timestamp()),
-        'end': int(datetime(2021, 11, 30).timestamp()),
-        'xmin': -65,
-        'xmax': -62,
-        'ymin': 43,
-        'ymax': 45,
-    }
 
+    # validate the request parameters
     need_keys = set(default_query.keys())
     recv_keys = set(http_qry.keys())
     missing = need_keys - recv_keys
@@ -60,11 +61,26 @@ def download():
         return Markup(f'Error: missing keys from request: {missing}<br>'
                       f'example:<br><code>{example_qry}<code>')
 
+    # convert parameter types from string
     http_qry['start'] = datetime.utcfromtimestamp(int(http_qry['start']))
     http_qry['end'] = datetime.utcfromtimestamp(int(http_qry['end']))
-
     for arg in ['xmin', 'xmax', 'ymin', 'ymax']:
         http_qry[arg] = float(http_qry[arg])
+
+    # error handling for invalid requests
+    if http_qry['end'] - http_qry['start'] > timedelta(days=31):
+        return Markup("Error: a maximum of 31 days can be queried at once")
+
+    if http_qry['end'] <= http_qry['start']:
+        return Markup("Error: end must occur after start")
+
+    if not (-180 <= http_qry['xmin'] < http_qry['xmax'] <= 180):
+        return Markup("Error: invalid longitude range")
+
+    if not (-90 <= http_qry['ymin'] < http_qry['ymax'] <= 90):
+        return Markup("Error: invalid longitude range")
+
+    download_name = f'ais_{http_qry["start"].date()}_{http_qry["end"].date()}.csv'
 
     with PostgresDBConn(**test_db_args) as dbconn:
 
@@ -73,24 +89,22 @@ def download():
         dbqry = DBQuery(dbconn=dbconn,
                         callback=aisdb.sqlfcn_callbacks.in_bbox_time_validmmsi,
                         **http_qry).gen_qry(verbose=False)
-        tracks = aisdb.TrackGen(dbqry, decimate=0.0001)
 
+        tracks = aisdb.TrackGen(dbqry, decimate=0.0001)
         try:
             aisdb.write_csv(tracks, buf)
-            buf.flush()
-            buf.seek(0)
-            send_file(buf,
-                      mimetype='csv',
-                      as_attachment=True,
-                      download_name='ais_test.csv',
-                      max_age=int(timedelta(days=7).total_seconds()))
-            buf.seek(0)
-            count = sum(1 for csvrow in buf)
-            return Markup(f"sent {count} rows to client")
-        except StopIteration:
+        except aisdb.track_gen.EmptyRowsException:
+            buf.close()
             return Markup("No results found for query")
         except Exception as err:
             raise err
+
+        buf.flush()
+        buf.seek(0)
+        count = sum(1 for line in buf)
+        print(f'sending {count} rows to client')
+        buf.seek(0)
+        return send_file(buf, as_attachment=True, download_name=download_name)
 
 
 if __name__ == '__main__':
